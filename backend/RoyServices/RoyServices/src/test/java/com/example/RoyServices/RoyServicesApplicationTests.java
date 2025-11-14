7c1b6deda68f6780dee5bcea0c9f0a9b07e7f4cf
jamaracortez <husniacortez1967@gmail.com>
2025-11-13 18:30:44 -0600

    nuevo

---
+++ backend/RoyServices/RoyServices/src/test/java/com/example/RoyServices/RoyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RoyServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
